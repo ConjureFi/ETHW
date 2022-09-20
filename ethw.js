c3d258753a8ba4f37fad5cfae1c3d1fb29fd6d30
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2022-09-20 08:36:21 +0100

    Add files via upload

--- ethw.js
+++ ethw.js
@@ -3,6 +3,7 @@ const ethers = require('ethers');
 require('dotenv').config()
 const ABI = require('./abi/oof.json')
 const {Contract} = require("ethers");
+const { formatUnits } = require("ethers/lib/utils");
  
 // config go to file later
 const rpc = process.env.RPC
@@ -18,17 +19,21 @@ const oofContract =  !!ABI && !!walletWithProvider
  
 async function getData() {
     let length = await oofContract.getFeedLength()
- 
-    console.log("Found Feeds:")
+ console.log("Contract address")
+ console.log(oofAddress)
+    console.log("Found Feeds:" + (formatUnits(length) * 10 ** 18))
+    console.log("########################################")
+
     for (var i = 0; i < length.toNumber(); i++) {
         let feedData = await oofContract.getFeedList([i])
         let feedInfo = await oofContract.getFeed(i)
  
         var d = new Date(0);
         d.setUTCSeconds(feedInfo[1]);
- 
+ console.log("Feed ID "+ i)
         console.log("Feed Name: " + feedData[0])
-        console.log("Feed Price Value: " + feedInfo[0])
+       console.log("Feed Price Value: " + feedInfo[0] / (10 ** feedData[1]))
+        console.log("Feed Price Value RAW: " + feedInfo[0])
         console.log("Feed Last Update: " + d)
         console.log("Feed Decimals: " + feedData[1])
         console.log("Feed Update Interval (s): " + feedData[2])
