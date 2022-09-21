2111c0d613067e1d70a4524cc8d3e3f9f9860df3
pr0toshi <46229158+pr0toshi@users.noreply.github.com>
2022-09-21 22:03:20 +0100

    Add files via upload

--- ethw.js
+++ ethw.js
@@ -23,22 +23,24 @@ async function getData() {
  console.log(oofAddress)
     console.log("Found Feeds:" + (formatUnits(length) * 10 ** 18))
     console.log("########################################")
-
+let n = [];
+for (var i = 0; i < length.toNumber(); i++){
+n.push(i);
+}let feedData = await oofContract.getFeedList(n)
+console.log(feedData);
+let feedInfo = await oofContract.getFeeds(n)
     for (var i = 0; i < length.toNumber(); i++) {
-        let feedData = await oofContract.getFeedList([i])
-        let feedInfo = await oofContract.getFeed(i)
- 
         var d = new Date(0);
-        d.setUTCSeconds(feedInfo[1]);
+        d.setUTCSeconds(feedInfo[1][i]);
  console.log("Feed ID "+ i)
-        console.log("Feed Name: " + feedData[0])
-       console.log("Feed Price Value: " + feedInfo[0] / (10 ** feedData[1]))
-        console.log("Feed Price Value RAW: " + feedInfo[0])
+        console.log("Feed Name: " + feedData[0][i])
+       console.log("Feed Price Value: " + feedInfo[0][i] / (10 ** feedData[1][i]))
+        console.log("Feed Price Value RAW: " + feedInfo[0][i])
         console.log("Feed Last Update: " + d)
-        console.log("Feed Decimals: " + feedData[1])
-        console.log("Feed Update Interval (s): " + feedData[2])
-        console.log("Feed Revenue Mode: " + feedData[3])
-        console.log("Feed Cost: " + feedData[4])
+        console.log("Feed Decimals: " + feedData[1][i])
+        console.log("Feed Update Interval (s): " + feedData[2][i])
+        console.log("Feed Revenue Mode: " + feedData[3][i])
+        console.log("Feed Cost: " + feedData[4][i])
         console.log("########################################")
     }
  
